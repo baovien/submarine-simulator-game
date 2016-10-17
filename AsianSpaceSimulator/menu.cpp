57742e9f840c6eedca84e2fa47e94d060bdc228d
Vien Bao Ngo <vienbn15@student.uia.no>
2016-10-17 12:32:11 +0200

    Change menu state

--- AsianSpaceSimulator/menu.cpp
+++ AsianSpaceSimulator/menu.cpp
@@ -1,86 +1,119 @@
-#include "menu.h"
+#include <iostream>
 #include "gamemode1.h"
-void main_menu::Initialize(sf::RenderWindow* window)
-{
+#include "menu.h"
+
+void main_menu::Initialize(sf::RenderWindow *window) {
+
     this->selected = 0;
+
     this->font = new sf::Font();
-    this->font->loadFromFile("Graphics/font.ttf");
-    this->title = new sf::Text("LETSGO", *this->font, 200U);
+    this->font->loadFromFile("graphics/font1.otf");
+
+    this->title = new sf::Text("SPACE", *this->font, 86U);
     this->title->setOrigin(this->title->getGlobalBounds().width / 2, this->title->getGlobalBounds().height / 2);
-    this->title->setPosition(window->getSize().x / 2, window->getSize().y / 8);
+    this->title->setPosition(window->getSize().x / 2, window->getSize().y / 4);
+
+    this->title2 = new sf::Text("SIMULATOR", *this->font, 86U);
+    this->title2->setOrigin(this->title2->getGlobalBounds().width / 2, this->title2->getGlobalBounds().height / 2);
+    this->title2->setPosition(window->getSize().x / 2, window->getSize().y / 4 + this->title2->getGlobalBounds().height);
 
     this->play = new sf::Text("Play", *this->font, 64U);
     this->play->setOrigin(this->play->getGlobalBounds().width / 2, this->play->getGlobalBounds().height / 2);
-    this->play->setPosition(window->getSize().x / 2, window->getSize().y / 2.1);
+    this->play->setPosition(window->getSize().x / 2, window->getSize().y / 2);
 
-    this->quit = new sf::Text("Quit", *this->font, 64U);
+    this->highscore = new sf::Text("Highscore", *this->font, 64U);
+    this->highscore->setOrigin(this->highscore->getGlobalBounds().width / 2, this->highscore->getGlobalBounds().height / 2);
+    this->highscore->setPosition(window->getSize().x / 2, window->getSize().y /2 + this->highscore->getGlobalBounds().height);
+
+    this->options = new sf::Text("Options", *this->font, 64U);
+    this->options->setOrigin(this->options->getGlobalBounds().width / 2, this->options->getGlobalBounds().height / 2);
+    this->options->setPosition(window->getSize().x / 2, window->getSize().y /2 + this->options->getGlobalBounds().height*2);
+
+    this->quit = new sf::Text("Exit", *this->font, 64U);
     this->quit->setOrigin(this->quit->getGlobalBounds().width / 2, this->quit->getGlobalBounds().height / 2);
-    this->quit->setPosition(window->getSize().x / 2, window->getSize().y - 100);
+    this->quit->setPosition(window->getSize().x / 2, window->getSize().y /2 + this->quit->getGlobalBounds().height*3);
 
 }
-void main_menu::Update(sf::RenderWindow* window)
-{
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up) && !this->upKey)
-    {
-        this->selected += 1;
-    }
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down) && !this->downKey)
-    {
+
+void main_menu::Update(sf::RenderWindow *window) {
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up) && !this->upKey){
         this->selected -= 1;
     }
-    if(this->selected > 1)
-    {
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down) && !this->downKey){
+        this->selected += 1;
+    }
+
+    if(this->selected > 3){
         this->selected = 0;
     }
-    if(this->selected < 0)
-    {
-        this->selected = 1;
+
+    if(this->selected < 0){
+        this->selected = 3;
     }
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Return))
-    {
-        switch(this->selected)
-        {
-            case 0:
-                coreState.SetState(new gamemode1());
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Return)){
+        switch(this->selected){
+            case 0: //Play
+                coreState.SetState(new gamemode1);
                 break;
-            case 1:
+            case 1: //Highscore
+                break;
+            case 2: //Options
+                break;
+            case 3: //Exit
                 quitGame = true;
                 break;
-            case 2:
-                // flere menyvalg
-                break;
-
         }
     }
+
     this->upKey = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up);
     this->downKey = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down);
 }
-void main_menu::Render(sf::RenderWindow* window)
-{
+
+void main_menu::Render(sf::RenderWindow *window) {
+
     this->play->setFillColor(sf::Color::White);
+    this->highscore->setFillColor(sf::Color::White);
+    this->options->setFillColor(sf::Color::White);
     this->quit->setFillColor(sf::Color::White);
+    this->play->setStyle(0);
+    this->highscore->setStyle(0);
+    this->options->setStyle(0);
+    this->quit->setStyle(0);
 
 
-    switch(this->selected)
-    {
+
+    switch(this->selected){
         case 0:
-            this->play->setFillColor(sf::Color::Green);
+            this->play->setStyle(1<<3);
             break;
         case 1:
-            this->quit->setFillColor(sf::Color::Green);
+            this->highscore->setStyle(1<<3);
             break;
         case 2:
-            //når vi har flere menyvalg er det er vi fyller inn fargen på valget når den er "selected" XD
+            this->options->setStyle(1<<3);
+            break;
+        case 3:
+            this->quit->setStyle(1<<3);
             break;
     }
     window->draw(*this->title);
+    window->draw(*this->title2);
     window->draw(*this->play);
+    window->draw(*this->highscore);
+    window->draw(*this->options);
     window->draw(*this->quit);
 }
-void main_menu::Destroy(sf::RenderWindow* window)
-{
+
+void main_menu::Destroy(sf::RenderWindow *window) {
     delete this->font;
     delete this->title;
+    delete this->title2;
     delete this->play;
+    delete this->highscore;
+    delete this->options;
     delete this->quit;
 }
+
--- AsianSpaceSimulator/menu.h
+++ AsianSpaceSimulator/menu.h
@@ -3,20 +3,28 @@
 #ifndef VIETNAMESE_SPACE_PROGRAM_MENU_H
 #define VIETNAMESE_SPACE_PROGRAM_MENU_H
 #include "state.h"
-class main_menu : public states
-{
+
+class main_menu : public states{
 public:
     void Initialize(sf::RenderWindow* window);
+
     void Update(sf::RenderWindow* window);
+
     void Render(sf::RenderWindow* window);
+
     void Destroy(sf::RenderWindow* window);
+
 private:
     sf::Font* font;
     sf::Text* title;
+    sf::Text* title2;
     sf::Text* play;
+    sf::Text* options;
+    sf::Text* highscore;
     sf::Text* quit;
 
     int selected;
-    bool upKey; bool downKey;
+
+    bool upKey, downKey;
 };
 #endif //VIETNAMESE_SPACE_PROGRAM_MENU_H
